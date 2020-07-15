1f669feb54db6587b2ec5130385ceffbe900d3c3
dogus <dogusdemirkiran@gmail.com>
2020-07-15 19:44:41 +0300

    PE DLL

---
+++ dllPE/dllPE.cpp
@@ -0,0 +1,78 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+
+int main(int argc, char* argv[]) {
+	const int MAX_FILEPATH = 255;
+	char fileName[MAX_FILEPATH] = { 0 };
+	memcpy_s(&fileName, MAX_FILEPATH, argv[1], MAX_FILEPATH);
+	HANDLE file = NULL;
+	DWORD fileSize = NULL;
+	DWORD bytesRead = NULL;
+	LPVOID fileData = NULL;
+	PIMAGE_DOS_HEADER dosHeader = {};
+	PIMAGE_NT_HEADERS imageNTHeaders = {};
+	PIMAGE_SECTION_HEADER sectionHeader = {};
+	PIMAGE_SECTION_HEADER importSection = {};
+	IMAGE_IMPORT_DESCRIPTOR* importDescriptor = {};
+	PIMAGE_THUNK_DATA thunkData = {};
+	DWORD thunk = NULL;
+	DWORD rawOffset = NULL;
+
+	// open file
+	file = CreateFileA(fileName, GENERIC_ALL, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (file == INVALID_HANDLE_VALUE) {
+		printf("Could not read file");
+	}
+
+	fileSize = GetFileSize(file, NULL);
+	fileData = HeapAlloc(GetProcessHeap(), 0, fileSize);
+	ReadFile(file, fileData, fileSize, &bytesRead, NULL);
+	// IMAGE_DOS_HEADER
+	dosHeader = (PIMAGE_DOS_HEADER)fileData;
+	
+	// IMAGE_NT_HEADERS
+	imageNTHeaders = (PIMAGE_NT_HEADERS)((DWORD)fileData + dosHeader->e_lfanew);
+	
+	DWORD sectionLocation = (DWORD)imageNTHeaders + sizeof(DWORD) + (DWORD)(sizeof(IMAGE_FILE_HEADER)) + (DWORD)imageNTHeaders->FileHeader.SizeOfOptionalHeader;
+	DWORD sectionSize = (DWORD)sizeof(IMAGE_SECTION_HEADER);
+
+	DWORD importDirectoryRVA = imageNTHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+
+	for (int i = 0; i < imageNTHeaders->FileHeader.NumberOfSections; i++) {
+		sectionHeader = (PIMAGE_SECTION_HEADER)sectionLocation;
+		
+		if (importDirectoryRVA >= sectionHeader->VirtualAddress && importDirectoryRVA < sectionHeader->VirtualAddress + sectionHeader->Misc.VirtualSize) {
+			importSection = sectionHeader;
+		}
+		sectionLocation += sectionSize;
+	}
+
+	rawOffset = ((DWORD)fileData + importSection->PointerToRawData);
+
+	importDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)(rawOffset + (imageNTHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress - importSection->VirtualAddress));
+
+	printf("\n******* DLL IMPORTS *******\n");
+	for (; importDescriptor->Name != 0; importDescriptor++) {
+		// imported dll modules
+		printf("\t%s\n", rawOffset + (importDescriptor->Name - importSection->VirtualAddress));
+		thunk = importDescriptor->OriginalFirstThunk == 0 ? importDescriptor->FirstThunk : importDescriptor->OriginalFirstThunk;
+		thunkData = (PIMAGE_THUNK_DATA)(rawOffset + (thunk - importSection->VirtualAddress));
+
+		// dll exported functions
+		for (; thunkData->u1.AddressOfData != 0; thunkData++) {
+			
+			if (thunkData->u1.AddressOfData > 0x80000000) {
+				
+				printf("\t\tOrdinal: %x\n", (WORD)thunkData->u1.AddressOfData);
+			}
+			else {
+				printf("\t\t%s\n", (rawOffset + (thunkData->u1.AddressOfData - importSection->VirtualAddress + 2)));
+			}
+		}
+	}
+
+	return 0;
+
+}
